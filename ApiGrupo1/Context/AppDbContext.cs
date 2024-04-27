02a68782d7a0fc7485be32cf83f2c865332af282
Marcelo <marcelo.cepeda17@gmail.com>
2024-04-27 20:48:48 -0300

    First commit of the WebApi project

---
+++ ApiGrupo1/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ApiGrupo1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiGrupo1.Context
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
