e120a9bde2b2b3d118f465cea709b2a597f9ac4d
Marcelo <marcelo.cepeda17@gmail.com>
2024-04-28 15:44:42 -0300

    Create user controller

--- ApiGrupo1/obj/Debug/net8.0/ApiGrupo1.AssemblyInfo.cs
+++ ApiGrupo1/obj/Debug/net8.0/ApiGrupo1.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiGrupo1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f93bb4370ea453b7594125c53fa8f4e962ebc8f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02a68782d7a0fc7485be32cf83f2c865332af282")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiGrupo1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiGrupo1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
